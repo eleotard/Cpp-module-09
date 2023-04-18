be1b3b9224811c8b4320d41f15bd36801bde04ec
elsie <eleotard@student.42.fr>
2023-04-18 19:53:19 +0200

    debut

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eleotard <eleotard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/18 18:57:18 by eleotard          #+#    #+#             */
+/*   Updated: 2023/04/18 19:02:37 by eleotard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class BitcoinExchange {
+    public:
+        BitcoinExchange();
+        BitcoinExchange(BitcoinExchange const& src);
+        ~BitcoinExchange();
+        BitcoinExchange const&operator=(BitcoinExchange const& src)
+    private:
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eleotard <eleotard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/18 18:57:00 by eleotard          #+#    #+#             */
+/*   Updated: 2023/04/18 19:42:24 by eleotard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "BitcoinExchange.hpp"
+
+int main(int argcm char **argv)
+{
+    if (argc <= 1)
+    {
+        std::cerr << "Error: could not open file." << std::endl;
+        return (1);
+    }
+    
+    return (0);
+}
