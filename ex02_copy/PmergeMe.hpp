f269b21165d3ceeadce451c03669e01c649de9c2
elsie <eleotard@student.42.fr>
2023-09-20 23:21:09 +0200

    ca y est g un plan

---
+++ ex02_copy/PmergeMe.hpp
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elsie <elsie@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 18:37:12 by eleotard          #+#    #+#             */
+/*   Updated: 2023/09/20 22:18:57 by elsie            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <sstream>
+#include <limits.h>
+#include <exception>
+
+# define YELLOW	"\033[33m"
+# define BLUE	"\033[34m"
+# define GREEN	"\033[32m"
+# define RED	"\033[31m"
+# define PURPLE	"\033[35m"
+# define DEFAULT    "\033[0m"
+
+typedef std::vector<std::vector<int > > t_vvect;
+typedef std::vector<int> t_vect;
+typedef std::vector<std::pair<int, int> > p_vect;
+typedef std::vector<std::pair<int, int> >::iterator p_iterator;
+
+
+class PmergeMe {
+	public:
+		class OOR : public std::exception {
+			virtual const char * what() const throw() {
+				return ("Error: input out of range (> INT_MAX)");
+			}
+		};
+		PmergeMe();
+		PmergeMe(char **argv);
+		~PmergeMe();
+		PmergeMe(PmergeMe const& src);
+		PmergeMe &operator=(PmergeMe const& src);
+		
+		void	printVect(t_vect list);
+		void	printPvect(p_vect pairs);
+		
+		t_vect	setVector(char **argv);
+		void	merge_insert(t_vect &list);
+		p_vect	makePairs(t_vect &list, int *solo);
+		void	sortPairs(p_vect &pairs);
+		t_vect	createNewList(p_vect &pairs);
+
+		void	insertionDichotomy(int nbtoInsert);
+		void	insertNbInMainChain(int nb, int index);
+		
+		t_vvect	getMainChain() const;
+
+		void	resetInsertionState();
+		int		findFirstNbPair(p_vect pairs, int first, int indexInMain);
+		t_vect	extractMainChainList();
+		t_vect	createVect(int a, int b);
+		
+	private:
+		t_vvect	_main_chain;
+		t_vect	_final_chain;
+};
+
+#endif
